3a96acb303e973281e15d340e72c01632098db0d
keepBlank <front-endligen@foxmail.com>
2023-07-07 16:31:36 +0800

    改写成TS

--- app2.ts
+++ app2.ts
@@ -1,15 +1,44 @@
-import express from 'express'
+import {RequestHandler} from 'express';
+import {ErrorRequestHandler} from 'express-serve-static-core';
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello,Express!');
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.post('/xxx',(req, res, next)=>{
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+} as RequestHandler);
 
-app.listen(port, () => {
-    console.log(`现在监听的端口是 ${port}`);
-});
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+    next()
+} as ErrorRequestHandler);
+
+module.exports = app;
--- routes/users.ts
+++ routes/users.ts
@@ -1,13 +1,15 @@
-var express = require('express');
-var router = express.Router();
+import {RequestHandler} from 'express';
+
+const  express = require('express');
+const router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.send('users 发送了这句话');
-});
+} as RequestHandler);
 
 // '/'的意思是/suers下的根路径
 router.get('/xxx', function(req, res, next) {
   res.send('xxx 发送了这句话');
-});
+} as RequestHandler);
 
 module.exports = router;
